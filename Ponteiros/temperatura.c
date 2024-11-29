1a2ec44411dcbdbe8dfe0fd6b962a4057a6db64b
Moisés <olimpiodev.tec@gmail.com>
2024-11-29 19:56:16 -0300

    Update temperatura.c

--- Ponteiros/temperatura.c
+++ Ponteiros/temperatura.c
@@ -1,31 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define TAMANHO_LINHA 100
+#include <string.h>
 
 int main()
 {
-    FILE *arquivo = fopen("C:\\Users\\Moises\\Desktop\\GitHub\\Estrutura-Dados-I-Aulas\\Ponteiros\\datalog.txt", "r");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
-        return 0;
-    }
-    
-    char linha[TAMANHO_LINHA];
-    char data[20];
+    FILE *arquivo = fopen("datalog.txt", "r");
+
+    char linha[100];
+    char data[30];
     float temperatura;
+    float menorTemperatura;
+    float maiorTemperatura;
+    char menorData[30];
+    char maiorData[30];
+    int contador = 0;
 
-    fgets(linha, TAMANHO_LINHA, arquivo);
+    fgets(linha, 100, arquivo);
 
-    while (fgets(linha, TAMANHO_LINHA, arquivo) != NULL)
+    while (fgets(linha, 100, arquivo) != NULL)
     {
-        int dadosLidos = sscanf(linha, "%s %f", data, &temperatura);
+        sscanf(linha, "%s %f", data, &temperatura);
 
-        printf("Data: %s, Temperatura: %.2f\n", data, temperatura);
+        if (contador == 0)
+        {
+            menorTemperatura = temperatura;
+            maiorTemperatura = temperatura;
+        }
+        else
+        {
+            if (temperatura < menorTemperatura)
+            {
+                menorTemperatura = temperatura;
+                strcpy(menorData, data);
+            }
+            if (temperatura > maiorTemperatura)
+            {
+                maiorTemperatura = temperatura;
+                strcpy(maiorData, data);
+            }
+        }
+        contador++;
     }
+
+    printf("Menor temperatura %.2f no dia %s\n", menorTemperatura, menorData);
+    printf("Maior temperatura: %.2f no dia %s\n", maiorTemperatura, maiorData);
+    
 
     fclose(arquivo);
-    
 
     return 0;
 }
