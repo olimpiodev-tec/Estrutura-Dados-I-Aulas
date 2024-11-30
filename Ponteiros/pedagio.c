b0deef8b1ad5e53d2f5b52cbb6a307eabe56dd4c
Moisés <olimpiodev.tec@gmail.com>
2024-11-29 21:20:11 -0300

    Update pedagio.c

--- Ponteiros/pedagio.c
+++ Ponteiros/pedagio.c
@@ -1,26 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
-int main()
+struct Pedagio {
+    float totalValoresPagos;
+    float totalValoresDescontados;
+};
+
+
+struct Pedagio pedagio;
+
+
+void imprimirRelatorio()
 {
-    FILE *arquivo = fopen("C:\\Users\\Moises\\Desktop\\GitHub\\Estrutura-Dados-I-Aulas\\Ponteiros\\pedagio.txt", "r");
+    printf("----------Relatório de pagamentos dos pedágios----------\n");
+    printf("Total de valores pagos: R$ %.2f\n", pedagio.totalValoresPagos);
+    printf("Total de valores descontados: R$ %.2f\n", pedagio.totalValoresDescontados);
+    printf("Saldo final do dia R$ %.2f\n", pedagio.totalValoresPagos - pedagio.totalValoresDescontados);
+}
 
-    if (arquivo == NULL)
-    {
-        printf("Erro ao ler arquivo");
-        return 0;
-    }
+
+void lerArquivo()
+{
+    FILE *arquivo = fopen("pedagio.txt", "r");
 
     char linha[100];
+    int cabine = 0;
+    float valor = 0;
+    float desconto = 0;
 
     fgets(linha, 100, arquivo);
 
     while (fgets(linha, 100, arquivo) != NULL)
     {
-        printf(linha);
+        sscanf(linha, "%i;%f;%f", &cabine, &valor, &desconto);
+        pedagio.totalValoresPagos += valor;
+        pedagio.totalValoresDescontados += desconto;
     }
-    
-    fclose(arquivo);
 
-    return 0; 
+    fclose(arquivo);
+}
+
+int main()
+{
+    setlocale(LC_ALL, NULL);
+    printf("Seja bem vindo ao sistema que opera os semáforos da cidade\n");
+    lerArquivo();
+    imprimirRelatorio();
+
+    return 0;
 }
